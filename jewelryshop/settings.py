f43d09db07d5888eec173cdd3bb8ac4883bab8a8
Galymzhan <bekgalym97@gmail.com>
2025-04-17 15:52:58 +0600

    Demo Production is ready, also Dockerfile, Docker-compose, Nginx, Redis

--- jewelryshop/settings.py
+++ jewelryshop/settings.py
@@ -7,13 +7,13 @@ import socket
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
-
+# SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = "j9w=_78!v$!_2k7r3g(t=wsp0$#u%-6f@lz0y7*p25)"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
-print("🔥 ALLOWED_HOSTS:", ALLOWED_HOSTS)
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
